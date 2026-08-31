55ac1dc45da91ef3c0b7319a904b45b8ca1793e9
gandhijanvi512-gif <gandhijanvi512@gmail.com>
2026-08-31 18:01:47 +0530

    changes footer route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -108,8 +108,8 @@ useEffect(() => {
       <Route path='/category/:category' element={<CategoryProducts />}/>
       <Route path='/editprofile' element={<EditProfile />}/>
       <Route path='/wishlist' element={<Wishlist />} />
-      <Route path='/about' element={<AboutUs />}/>
-      <Route path="/contact" element={<ContactUs />}/>
+      <Route path='/aboutus' element={<AboutUs />}/>
+      <Route path="/contactus" element={<ContactUs />}/>
       
       
       
