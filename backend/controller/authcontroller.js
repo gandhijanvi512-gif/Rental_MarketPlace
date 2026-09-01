e93d31add86f89d4831b81fb7b3c1feaf8a210e3
gandhijanvi512-gif <gandhijanvi512@gmail.com>
2026-09-01 10:44:33 +0530

    fixed login issue

--- backend/controller/authcontroller.js
+++ backend/controller/authcontroller.js
@@ -100,7 +100,7 @@ export const signin=async(req,res)=>{
         res.cookie("refreshtoken", refreshtoken, {
           httpOnly: true,
           secure: true,
-          sameSite: "none",
+          sameSite: "lax",
           path: "/",
           maxAge: 7 * 24 * 60 * 60 * 1000,
         });
